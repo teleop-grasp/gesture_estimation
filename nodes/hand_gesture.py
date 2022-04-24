64d1598f9d06a50be0e46383f4737af52c0b65b8
Slaxzer <peter@qrholm.dk>
2022-04-24 21:52:46 +0200

    convert feed to opencv img

--- nodes/hand_gesture.py
+++ nodes/hand_gesture.py
@@ -4,10 +4,7 @@ from gesture_estimation import get_gesture
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
-
-# cap = cv2.VideoCapture(0)
-# cap.set(3, 1280)
-# cap.set(4, 720)
+from cv_bridge import CvBridge
 
 if __name__=="__main__":
 
@@ -15,16 +12,18 @@ if __name__=="__main__":
     params = rospy.get_param("~")
     rospy.loginfo(params)
     pub_gesture = rospy.Publisher(params["topic_grasp_state"], Bool, queue_size=10)
-
-    # if not cap.isOpened():
-    #     print(f"cant find video feed!!!!")
-        
+    bridge = CvBridge()
+    
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        # Get image frame
-        img = rospy.wait_for_message(params["topic_image_raw"], Image)
-        
-        gesture = get_gesture("", img)
-        print(f"Current gesture: {gesture}")
+
+        # Get image msg
+        img_msg = rospy.wait_for_message(params["topic_image_raw"], Image)
+
+        # Convert to openCV format
+        cv_img = bridge.imgmsg_to_cv2(img_msg)
+        gesture = get_gesture("", cv_img)
+
+        # print(f"Current gesture: {gesture}")
         pub_gesture.publish(gesture)
         rate.sleep()
