94d46b5234adf2ab6c0b9ef6c7e2408c87c42e45
Martin <martinandrovich@gmail.com>
2022-04-25 20:34:36 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!usr/bin/env python3
 
+# python ROS package
+# http://www.artificialhumancompanions.com/structure-python-based-ros-package/
+
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['gesture_estimation'],
-     package_dir={'': 'src'}
+	packages=["gesture_estimation"],
+	package_dir={"": "src"},
+	install_requires=[
+		"opencv-python"
+		"cvzone",
+	]
 )
 
 setup(**setup_args)
