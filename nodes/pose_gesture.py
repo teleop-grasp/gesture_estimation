c4daf20568a060c425fe4ffae8c2168ece09418d
Martin <martinandrovich@gmail.com>
2022-05-02 16:14:20 +0200

    code review - add tcp_nodelay to publishers - convert spaces to tabs - change to double quotes - add some spacing - trim trailing whitespace - rename "visualize_tracking" - lowercase comments

--- nodes/pose_gesture.py
+++ nodes/pose_gesture.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Pose
 from cv_bridge import CvBridge
-from numpy_ros import to_message 
+from numpy_ros import to_message
+
 from hand_tracking.gesture_estimation import get_gesture
 from hand_tracking.plot_utils import draw_hand_lms, Pose3DViewer
 from hand_tracking.tracking import track_hands, track_body
@@ -12,54 +14,57 @@ from hand_tracking.pose_estimation import get_pose
 
 if __name__=="__main__":
 
-    rospy.init_node("pose_gesture")
-    params = rospy.get_param("~")
-    rospy.loginfo(params)
-    topic_grasp_state = params["topic_grasp_state"]
-    topic_pose_hand = params["topic_pose_hand"]
-    topic_image_hand = params["topic_image_hand"]
-    show_visualizations = params["show_visualizations"]
+	rospy.init_node("pose_gesture")
 
-    pub_gesture = rospy.Publisher(topic_grasp_state, Bool, queue_size=1)
-    pub_pose = rospy.Publisher(topic_pose_hand, Pose, queue_size=1)
+	# read node parameters
+	params = rospy.get_param("~")
+	topic_grasp_state = params["topic_grasp_state"]
+	topic_pose_hand = params["topic_pose_hand"]
+	topic_image_hand = params["topic_image_hand"]
+	topic_image_raw = params["topic_image_raw"]
+	visualize_tracking = params["visualize_tracking"]
 
-    if show_visualizations:
-        pub_img_hand = rospy.Publisher(topic_image_hand, Image, queue_size=1)
+	pub_gesture = rospy.Publisher(topic_grasp_state, Bool, queue_size=1, tcp_nodelay=True)
+	pub_pose = rospy.Publisher(topic_pose_hand, Pose, queue_size=1, tcp_nodelay=True)
 
-    bridge = CvBridge()
-    hand_type = 'right'
-    Pose3DViewer = Pose3DViewer()
+	if visualize_tracking:
+		pub_img_hand = rospy.Publisher(topic_image_hand, Image, queue_size=1)
 
-    rate = rospy.Rate(30) # 30hz
-    while not rospy.is_shutdown():
-        # Get image msg
-        img_msg = rospy.wait_for_message(params["topic_image_raw"], Image)
+	bridge = CvBridge()
+	hand_type = "right"
+	Pose3DViewer = Pose3DViewer()
 
-        # Convert to openCV format
-        cv_img = bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
+	rate = rospy.Rate(30) # hz
+	while not rospy.is_shutdown():
+		# get image msg
+		img_msg = rospy.wait_for_message(topic_image_raw, Image)
 
-        hands = track_hands(cv_img)    
-        body = track_body(cv_img)
+		# convert to opencv format
+		cv_img = bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
 
-        if hand_type in hands:
-            # Get desired hand
-            hand = hands[hand_type]
-            
-            gesture = get_gesture(hand)
-            
-            # Add hand landmarks to img 
-            cv_img = draw_hand_lms(cv_img, hand)
+		hands = track_hands(cv_img)
+		body = track_body(cv_img)
 
-            if body is not None:
-                pose = get_pose(hand, body)
-                pose_msg = to_message(Pose, pose)
-                pub_pose.publish(pose_msg)
+		if hand_type in hands:
+			# get desired hand
+			hand = hands[hand_type]
 
-                if show_visualizations:
-                    Pose3DViewer.plot_pose(pose, hand)
+			gesture = get_gesture(hand)
 
-            pub_gesture.publish(gesture)
+			# add hand landmarks to img
+			cv_img = draw_hand_lms(cv_img, hand)
 
-            if show_visualizations:
-                pub_img_hand.publish(bridge.cv2_to_imgmsg(cv_img, encoding="bgr8"))
-        rate.sleep()
+			if body is not None:
+				pose = get_pose(hand, body)
+				pose_msg = to_message(Pose, pose)
+				pub_pose.publish(pose_msg)
+
+				if visualize_tracking:
+					Pose3DViewer.plot_pose(pose, hand)
+
+			pub_gesture.publish(gesture)
+
+			if visualize_tracking:
+				pub_img_hand.publish(bridge.cv2_to_imgmsg(cv_img, encoding="bgr8"))
+
+		rate.sleep()
--- src/hand_tracking/tracking.py
+++ src/hand_tracking/tracking.py
@@ -3,66 +3,67 @@ import mediapipe as mp
 import numpy as np
 from scipy.spatial.transform import Rotation
 
+# body tracking
 mp_body_pose = mp.solutions.pose
+body_tracker = mp_body_pose.Pose(
+	static_image_mode=False,
+	model_complexity=2,
+	smooth_landmarks=True,
+	enable_segmentation=False,
+	smooth_segmentation=True,
+	min_detection_confidence=0.5,
+	min_tracking_confidence=0.5
+)
 
-body_tracker = mp_body_pose.Pose(    
-    static_image_mode=False,
-    model_complexity=2,
-    smooth_landmarks=True,
-    enable_segmentation=False,
-    smooth_segmentation=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5)
+# hand + gesture tracking
+mp_hands = mp.solutions.hands
+hand_tracker = mp_hands.Hands(static_image_mode=False,
+						max_num_hands=2,
+						model_complexity=1,
+						min_detection_confidence=0.7,
+						min_tracking_confidence=0.7)
 
+# rotation matrix to align pts
+R = Rotation.from_euler("yxz", [-np.pi/2, -np.pi/2, 0]).as_matrix()
 
 def track_body(img):
-    img.flags.writeable = False
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = body_tracker.process(img)
-      
-    body = {'lms_pose': None, 'lms_pose_world': None}
+	img.flags.writeable = False
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	results = body_tracker.process(img)
 
-    if results.pose_landmarks:
-        body['lms_pose'] = np.array([(lm.x, lm.y, lm.z, lm.visibility) for lm in results.pose_landmarks.landmark])
-        body['lms_pose_world'] = np.array([(lm.x, lm.y, lm.z, lm.visibility) for lm in results.pose_world_landmarks.landmark])
-        return body
-    return None
+	body = {"lms_pose": None, "lms_pose_world": None}
 
+	if results.pose_landmarks:
+		body["lms_pose"] = np.array([(lm.x, lm.y, lm.z, lm.visibility) for lm in results.pose_landmarks.landmark])
+		body["lms_pose_world"] = np.array([(lm.x, lm.y, lm.z, lm.visibility) for lm in results.pose_world_landmarks.landmark])
+		return body
 
-
-
-mp_hands = mp.solutions.hands
-hand_tracker = mp_hands.Hands(static_image_mode=False, 
-                        max_num_hands=2,
-                        model_complexity=1,
-                        min_detection_confidence=0.7,
-                        min_tracking_confidence=0.7)
-        
-R = Rotation.from_euler('yxz', [-np.pi/2, -np.pi/2,  0]).as_matrix()
+	else:
+		return None
 
 def track_hands(img, flip_type=True) -> dict:
-    img.flags.writeable = False
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hand_tracker.process(img)
-  
-    hands = {}
-    if not results.multi_handedness:
-        return hands
+	img.flags.writeable = False
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	results = hand_tracker.process(img)
 
-    for handness, hand_landmarks, hand_world_landmarks in zip(results.multi_handedness, results.multi_hand_landmarks, results.multi_hand_world_landmarks):        
-        hand_type = handness.classification[0].label.lower()
-        hand_type = hand_type if not flip_type else ("left" if hand_type == "right" else "right") # Flip for mirrored view for mediapipe 
-                                         
-        hand_img = [(landmark.x, landmark.y, landmark.z) for landmark in hand_landmarks.landmark]
-        lms_world = np.array([(landmark.x, landmark.y, landmark.z) for landmark in hand_world_landmarks.landmark])
-        lms_world = (R @ lms_world.T).T
-        
-        hand = {}
-        hand['lms_media_pipe'] = hand_landmarks
-        hand['lms_img'] = hand_img
-        hand['lms_world'] = lms_world
-        hand['type'] = hand_type
+	hands = {}
+	if not results.multi_handedness:
+		return hands
 
-        hands[hand_type] = hand
+	for handness, hand_landmarks, hand_world_landmarks in zip(results.multi_handedness, results.multi_hand_landmarks, results.multi_hand_world_landmarks):
+		hand_type = handness.classification[0].label.lower()
+		hand_type = hand_type if not flip_type else ("left" if hand_type == "right" else "right") # flip for mirrored view for mediapipe
 
-    return hands      
+		hand_img = [(landmark.x, landmark.y, landmark.z) for landmark in hand_landmarks.landmark]
+		lms_world = np.array([(landmark.x, landmark.y, landmark.z) for landmark in hand_world_landmarks.landmark])
+		lms_world = (R @ lms_world.T).T
+
+		hand = {}
+		hand["lms_media_pipe"] = hand_landmarks
+		hand["lms_img"] = hand_img
+		hand["lms_world"] = lms_world
+		hand["type"] = hand_type
+
+		hands[hand_type] = hand
+
+	return hands
