be1fe7f86d441c0c7fb23cfd9c226abd9736b551
Martin <martinandrovich@gmail.com>
2022-05-07 22:36:45 +0200

    tempory tracking

--- nodes/pose_gesture.py
+++ nodes/pose_gesture.py
@@ -13,6 +13,10 @@ from hand_tracking.plot_utils import draw_hand_lms, Pose3DViewer
 from hand_tracking.tracking import track_hands, track_body
 from hand_tracking.pose_estimation import get_pose, calibrate
 
+from geometry_msgs.msg import PoseStamped
+from scipy.spatial.transform import Rotation
+import numpy as np
+
 if __name__=="__main__":
 
 	rospy.init_node("pose_gesture")
@@ -37,6 +41,7 @@ if __name__=="__main__":
 	# publishers
 	pub_gesture = rospy.Publisher(topic_grasp_state, Bool, queue_size=1, tcp_nodelay=True)
 	pub_pose = rospy.Publisher(topic_pose_hand, Pose, queue_size=1, tcp_nodelay=True)
+	pub_ee = rospy.Publisher("/cartesian_admittance_controller/command", PoseStamped, queue_size=1, tcp_nodelay=True)
 
 	# subsriber to camera image
 	img_msg = rospy.wait_for_message(topic_image_raw, Image)
@@ -83,6 +88,25 @@ if __name__=="__main__":
 					rospy.logwarn_once("calibrating depth normalization...")
 					rospy.logwarn_once("move your hand as far back and then as far forward as possible...")
 					calibrate(pose, max_num_readings=100)
+
+				# publish to robot
+				pose_ee = PoseStamped()
+				R_ee = Rotation.from_quat([0.92388, -0.382683, 0, 0])
+				R_d = Rotation.from_matrix(pose[0:3, 0:3])
+				R_ee_d = R_ee * R_d * Rotation.from_rotvec([0, 0, np.pi/2])
+				quat_d = R_ee_d.as_quat() # [x, y, z, w]
+
+				pose_ee.pose.position = pose_msg.position
+				pose_ee.pose.position.y = pose_ee.pose.position.y * 2 - 1
+				pose_ee.pose.position.y = max(min(pose_ee.pose.position.y, 0.25), -0.25)
+				pose_ee.pose.position.z = max(pose_ee.pose.position.z, 0.5)
+
+				pose_ee.pose.orientation.x = quat_d[0]
+				pose_ee.pose.orientation.y = quat_d[1]
+				pose_ee.pose.orientation.z = quat_d[2]
+				pose_ee.pose.orientation.w = quat_d[3]
+
+				pub_ee.publish(pose_ee)
 
 			if visualize_tracking:
 				g_pose, g_hand = pose, hand
